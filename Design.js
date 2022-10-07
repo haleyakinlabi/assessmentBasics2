a0f6cc07d3085649399cef2b2fe492b1615cdbc8
Haley Akin <wasatchtimpanogos@outlook.com>
2022-10-07 12:49:11 -0600

    ran out of time on the test

---
+++ Design.js
@@ -0,0 +1,1 @@
+//https://www.figma.com/file/42nGJF74gBHPOeOzprxTGb/Part-2%3A-Design-(Assessment-2)?node-id=0%3A1
---
+++ menu.js
@@ -0,0 +1,218 @@
+///////////////////////////////////////////////
+///////////////////MENU.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code that
+    deals with food objects, arrays of objects
+    and filtering those arrays. 
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Create an object called `pizza` that has 6
+    properties: 
+        - name (string)
+        - price (number)
+        - category (string)
+        - popularity (number)
+        - rating (number)
+        - tags (array of strings)
+
+    Make sure that you give your properties values
+    of the correct data type.
+
+    Note: the category is something like appetizer/entree,
+    the popularity is an overall ranking, the
+    rating is an average of all customer ratings, and
+    for the tags, think of things that a
+    user might filter by, like 'gluten-free' or
+    'kids'
+*/
+
+//CODE HERE
+let pizza ={
+    name: 'pepperoni',
+    price: 10,
+    category: 'entree',
+    popularity: 60,
+    rating: 90,
+    tags: ['for meat lovers', 'has gluten', 'lots of carbs']
+}
+
+
+
+//////////////////PROBLEM 2////////////////////
+/* 
+    Let's print a few values from our pizza object.
+
+    First, log the popularity of pizza. 
+    Use dot notation to access the value.
+*/
+
+//CODE HERE
+//console.log(pizza.popularity)
+let popularityValue = pizza.popularity
+console.log(popularityValue)
+/*
+    Second, log the second tag in your pizza's
+    tags array.
+    Use a combination of dots and brackets to
+    get the value.
+*/
+
+//CODE HERE
+    //let sndTagValue = pizza.popularity
+    //for(i = 0; i < tags.length; i++){
+    
+    //}
+    //console.log(sndTagValue)
+/*
+    Third, destructure the price off of the
+    pizza object.
+    
+    Print the value of your new price variable.
+*/
+
+//CODE HERE
+let pizzaPriceValue = pizza.price
+console.log(pizzaPriceValue)
+
+
+/*
+    Fourth, and last, destructure the category
+    property.
+
+    Print the value of your category variable. 
+*/
+
+//CODE HERE
+
+
+
+//////////////////PROBLEM 3////////////////////
+/* 
+    Create an array with about 5 objects in it.
+    The objects should mimic the `pizza` object.
+    Call the array `foodArr`.
+
+    Make sure that they have slightly different
+    values for price, popularity, rating, and
+    tags. That way, you'll be able to use this
+    data in some functions that you'll write.
+*/
+
+//CODE HERE
+let foodArr = [{
+    name: 'pizza',
+    price: 10,
+    category: 'entree',
+    popularity: 60,
+    rating: 90,
+    tags: ['for meat lovers', 'has gluten', 'lots of carbs']
+},{
+    name: 'noodles',
+    price: 8,
+    category: 'entree',
+    popularity: 90,
+    rating: 80,
+    tags: ['has gluten, for kids', 'for pasta lovers']
+},{
+    name: 'rice',
+    price: 5,
+    category: 'side',
+    popularity: 30,
+    rating: 50,
+    tags: ['gluten free', 'for entrees']
+},{
+    name: 'corn',
+    price: 6,
+    category: 'side',
+    popularity: 0,
+    rating: 10,
+    tags: ['gluten free', 'side', 'for kids']
+},{
+    name: 'chicken wings',
+    price: 11,
+    category: 'appitizer',
+    popularity: 100,
+    Rating: 90,
+    tags: ['bone in', 'for adults', 'gluten free']
+
+}
+]
+
+//////////////////PROBLEM 4////////////////////
+/* 
+    Let's filter the food objects according
+    to their tags.
+
+    Write a callback function below that will
+    return only food objects that have a certain tag.
+
+    You can check for any tag that at least 1 of
+    your food objects has.
+*/
+
+//CODE HERE
+
+let filteredFoodFun = foodArr.filter(function(element){
+    return element === 'gluten free'
+})
+console.log(filteredFoodFun)
+
+
+
+
+
+//////////////////PROBLEM 5////////////////////
+/* 
+    Now let's write a function that's a little
+    more flexible than just filtering for one
+    value. We want to be able to filter for 
+    food that has above a certain rating, 
+    below a certain price, or any other combo.
+
+    Write a function called `filterByProperty`
+    that takes in three arguments: `property`, 
+    `number`, and `type. 
+
+    The property will be a string (rating,
+    popularity, or price)
+
+    The number will be the number that you want
+    to compare against 
+
+    The type should be 'above' or 'below' to 
+    indicate whether you want to get foods with
+    values that are above or below the given number
+    for the given property
+
+    Inside the function, create a variable to hold
+    a filtered array
+
+    Use the filter method to filter the foodArr
+
+        In the callback, check if the `type` is `above`, 
+        if it is, return objects whose value for the given
+        property is greater than the `number` passed in
+
+        If the type isn't `below`, return objects whose
+        value for the given property is less than the 
+        `number` passed in
+    
+    Return the filtered array from the entire function
+*/
+
+//CODE HERE
+//ran out of time
+
+/*
+    Invoke the `filterByProperty` function passing
+    in a value for each paramter.
+
+    You'll have to console.log to see the filtered array
+*/
+
+//CODE HERE
+//ran out of time
